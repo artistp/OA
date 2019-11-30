039bf01b758c7371a865d6cdd224fc0f85f4702c
Heaven <artistp@163.com>
2019-11-30 22:32:49 +0800

    add AdminimService

---
+++ src/main/java/com/thzw/oa/controller/MissionController.java
@@ -0,0 +1,99 @@
+//package com.thzw.oa.controller;
+//
+//import com.thzw.oa.entity.find_members_by_adminid;
+//import com.thzw.oa.entity.userbaseinfo;
+//import com.thzw.oa.service.AdminimService;
+//import com.thzw.oa.service.MissionService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import com.thzw.oa.entity.adminiminfo;
+//
+//import javax.servlet.http.HttpSession;
+//import java.util.Date;
+//import java.util.List;
+//
+//
+//@Controller
+//@RequestMapping("/captain")
+//public class MissionController {
+//
+//    @Autowired
+//    private MissionService missionService;
+//
+//    @Autowired
+//    private AdminimService adminimService;
+//
+//    @RequestMapping("/MappingView")
+//    public String captainMapping()
+//    {
+//        return "captain_mappingmates.html";
+//    }
+//
+//
+//    @RequestMapping("/selectMapping")
+//    @ResponseBody
+//    public List<find_members_by_adminid> selectMapping(HttpSession session)
+//    {
+//        // 获取session   已登录的用户的账户session
+//        userbaseinfo logineduser = (userbaseinfo)session.getAttribute("LoginedUser");
+//        if( logineduser == null)
+//            return null;
+//        long id = logineduser.getAccreditedvillageid();
+//        List<find_members_by_adminid> findMembersByAdminids = missionService.getmates(6);
+//        return findMembersByAdminids;
+//    }
+//
+//
+//    @RequestMapping("/distMission")
+//    @ResponseBody
+//    public String distMission(adminiminfo info, HttpSession session)
+//    {
+//        // 获取session   已登录的用户的账户session
+//        userbaseinfo logineduser = (userbaseinfo)session.getAttribute("LoginedUser");
+//        if( logineduser == null)
+//            return null;
+//        long id = logineduser.getAccreditedvillageid();
+//
+//        // 先发布任务
+//        adminiminfo inf = adminimService.addMission(info ,id);
+//        // 任务发布到队员
+//        List<find_members_by_adminid> findMembersByAdminids = missionService.getmates(id);
+//        adminimService.missionToUser(inf, findMembersByAdminids);
+//        return "OK";
+//    }
+//
+//    @RequestMapping("/test")
+//    @ResponseBody
+//    public String test(HttpSession session)
+//    {
+//        /*
+//        // 获取session   已登录的用户的账户session
+//        userbaseinfo logineduser = (userbaseinfo)session.getAttribute("LoginedUser");
+//        if( logineduser == null)
+//            return null;
+//
+//         */
+//        adminiminfo test = new adminiminfo();
+//
+//        test.setTitle("title");
+//        test.setImcontent("IMContent");
+//        test.setStime(new Date());
+//        test.setEtime(new Date());
+//        test.setUserid((long)6);
+//        Byte imtype = 0b01;
+//        test.setImtype(imtype);
+//
+//
+//
+//        long id = 6; //logineduser.getAccreditedvillageid();
+//
+//        // 先发布任务
+//        adminiminfo inf = adminimService.addMission(test ,id);
+//        // 任务发布到队员
+//        List<find_members_by_adminid> findMembersByAdminids = missionService.getmates(id);
+//        adminimService.missionToUser(inf, findMembersByAdminids);
+//        return "OK";
+//    }
+//}
---
+++ src/main/java/com/thzw/oa/service/AdminimService.java
@@ -0,0 +1,75 @@
+package com.thzw.oa.service;
+
+import com.thzw.oa.entity.adminiminfo;
+import com.thzw.oa.entity.adminiminfoExample;
+import com.thzw.oa.entity.find_members_by_adminid;
+import com.thzw.oa.entity.imtomember;
+import com.thzw.oa.mapper.adminiminfoMapper;
+import com.thzw.oa.mapper.imtomemberMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class AdminimService implements AdminimS {
+
+    @Autowired
+    private adminiminfoMapper adminiminfoMapper;
+
+    @Autowired
+    private imtomemberMapper imtomemberMapper;
+
+    public adminiminfo addMission(String title, String IMContent, Date Stime, Date Etime , long UserId)
+    {
+        adminiminfoExample example = new adminiminfoExample();
+        int count = adminiminfoMapper.countByExample( example );
+
+        adminiminfo info = new adminiminfo();
+
+        info.setImid( count+1 );
+        info.setTitle(title);
+        info.setImcontent(IMContent);
+        info.setStime(Stime);
+        info.setEtime(Etime);
+        info.setUserid(UserId);
+        Byte imtype = 0b01;
+        info.setImtype(imtype);
+        int ret = adminiminfoMapper.insert(info);
+
+        return info;
+    }
+
+    public adminiminfo addMission(adminiminfo info ,long id)
+    {
+        adminiminfoExample example = new adminiminfoExample();
+        int count = adminiminfoMapper.countByExample( example );
+
+        info.setImid( count+1 );
+        Byte imtype = 0b01;
+
+        info.setImtype(imtype);
+        int ret = adminiminfoMapper.insert(info);
+
+        return info;
+    }
+
+    public int missionToUser(adminiminfo inf, List<find_members_by_adminid> findM)
+    {
+        int missionID = inf.getImid();
+        long captainID = inf.getUserid();
+        Byte check = 0;
+        for(int i =0;i <findM.size();i++)
+        {
+            imtomember imtomem = new imtomember();
+            imtomem.setIfcheck(check);
+            imtomem.setImid(missionID);
+            find_members_by_adminid mem = findM.get(i);
+            long memID = mem.getMemberId();
+            imtomem.setTouserid(memID);
+            imtomemberMapper.insert( imtomem );
+        }
+        return 1;
+    }
+}
